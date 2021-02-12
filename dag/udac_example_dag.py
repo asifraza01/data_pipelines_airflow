9177fdb85e0e15da3648e4a88e28a0a3a65b23f4
asifraza01 <78961965+asifraza01@users.noreply.github.com>
2021-02-12 15:57:26 +0200

    Update udac_example_dag.py

--- dag/udac_example_dag.py
+++ dag/udac_example_dag.py
@@ -1,1 +1,150 @@
 
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator
+                               #,PostgresOperator
+                              )
+from helpers import SqlQueries
+#import datetime
+import logging
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2021, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False,
+    'catchup': False
+    
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='@hourly'
+          
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+
+
+
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udac001",
+    s3_key="log_asif",
+    copy_json_option='s3://udac001/log_json_path.json',
+    region='eu-north-1'
+    
+)
+
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    table="staging_songs  ",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udac001",
+    s3_key="song_asif",
+    copy_json_option='auto',
+    region='eu-north-1'
+    
+    
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songplays',
+    select_sql=SqlQueries.songplay_table_insert
+    
+    
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='users',
+    truncate=True,
+    select_sql=SqlQueries.user_table_insert
+    
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songs',
+    truncate=True,
+    select_sql=SqlQueries.song_table_insert
+    
+    
+    
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+
+    redshift_conn_id='redshift',
+    table='artists',
+    select_sql=SqlQueries.artist_table_insert,
+    truncate=True,
+    primary_key="artistid"
+
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='time',
+    truncate=True,
+    select_sql=SqlQueries.time_table_insert
+    
+    
+    
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    tables=['songplays', 'users', 'songs', 'artists', 'time'],
+    redshift_conn_id="redshift"
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+#start_operator >> create_tables
+start_operator>> stage_events_to_redshift
+start_operator>>stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_song_dimension_table 
+load_songplays_table >> load_artist_dimension_table 
+load_songplays_table >> load_time_dimension_table
+load_songplays_table >> load_user_dimension_table
+
+load_songplays_table>> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
+
